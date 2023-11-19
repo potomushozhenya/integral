2333e2b519aac44cb169b2c5200310b475472c31
potomushozhenya <zhestokanov2017@yandex.ru>
2023-11-19 22:32:34 +0300

    refresh logics and add graphics for some values

--- main.py
+++ main.py
@@ -97,15 +97,20 @@ def quadratureFormulaGauss(leftBorder, rightBorder, nodeNumber):
             curr.append(moments[j+i])
         left_part.append(curr)
     a = np.linalg.solve(left_part, right_part)
+    #print(left_part)
+    #print(right_part)
     poly = [1]
     for i in range(nodeNumber):
         poly.append(a[nodeNumber-i-1])
+    #print(poly)
     roots = np.sort(np.roots(poly))
+    #print(roots)
     #for root in roots:
     #    if root < leftBorder or root > rightBorder:
     #        return None
     X = np.transpose(np.vander(roots, increasing=True))
     A = np.linalg.solve(X, moments[:nodeNumber])
+    #print(A)
     #for a in A:
     #    if a < 0:
     #        return None
@@ -163,12 +168,12 @@ def printQFGauss(nodesNumber):
 
 def compoundQuadratureFormulas(nodeNumber, pointsPerSection, flag_is_newton):
     result = 0
-    nodeList = nodeEquidistant(1.8, 2.3, nodeNumber)
+    nodeList = nodeEquidistant(1.8, 2.3, nodeNumber + 1)
     if flag_is_newton:
-        for i in range(nodeNumber-1):
+        for i in range(nodeNumber):
             result += interpolationQuadratureFormula(nodeList[i], nodeList[i+1], pointsPerSection, nodeEquidistant)[0]
     else:
-        for i in range(nodeNumber-1):
+        for i in range(nodeNumber):
             result += quadratureFormulaGauss(nodeList[i], nodeList[i+1], pointsPerSection)[0]
     return result
 
@@ -182,35 +187,74 @@ def ruleRunge(m, l, sum_h1, sum_h2, h_form, eps):
 
 
 def optimalIntegral(h0, l, flag_is_newton, eps):
-    #bigMesh булевая переменная отвечающая за сетки если true то h b h/l если false то h/l и h/l^2
-    if flag_is_newton:
-        pointsPerSection = 3
-    else:
-        pointsPerSection = 4
+    #if flag_is_newton:
+    #    pointsPerSection = 3
+    #else:
+    #    pointsPerSection = 4
+    pointsPerSection = 3
     h_form = math.ceil(0.5/h0)
-    s = [compoundQuadratureFormulas(math.ceil(h_form * (l**i)), pointsPerSection, flag_is_newton) for i in range(3)]
-    mPrev = 0
+    h = [h_form]
+    s = [compoundQuadratureFormulas(h_form * (l**i), pointsPerSection, flag_is_newton) for i in range(3)]
+    m = [0]
+    cm1 = [0]
+    cm2 = [0]
+    #Написать графики m, cm и значений на сетках относительно шага
     while True:
-        m = processAitken(s[0], s[1], s[2], l)
-        print(h_form*l*l, m, integralCorrectMean - s[2])
-        if abs(mPrev - m) < 0.15:
+        m.append(processAitken(s[-3], s[-2], s[-1], l))
+        if not math.isnan(m[-1]):
+            cm1.append((s[-2] - s[-3]) / ((1 - l ** (-m[-1])) * (math.ceil(h_form / 0.5)) ** m[-1]))
+            cm2.append((s[-1] - s[-2]) / ((1 - l ** (-m[-1])) * (math.ceil((h_form * l) / 0.5)) ** m[-1]))
+        else:
+            cm1.append(0)
+            cm2.append(0)
+        print(h_form*l*l, m[-1], s[-3] - integralCorrectMean, s[-2] - integralCorrectMean, s[-1] - integralCorrectMean)
+        if abs(m[-2] - m[-1]) < 0.15:
             break
-        mPrev = m
         h_form = h_form * l
-        s[0] = s[1]
-        s[1] = s[2]
-        s[2] = compoundQuadratureFormulas(h_form * l * l, pointsPerSection, flag_is_newton)
-    h_opt = math.ceil(ruleRunge(m, l, s[0], s[1], h_form, eps))
+        h.append(h_form)
+        s.append(s[-2])
+        s.append(s[-2])
+        s.append(compoundQuadratureFormulas(h_form * l * l, pointsPerSection, flag_is_newton))
+    h_opt = math.ceil(ruleRunge(m[-1], l, s[-3], s[-2], h_form, eps))
+    h.append(h_opt)
     print(h_opt)
-    s[2] = compoundQuadratureFormulas(h_opt, pointsPerSection, flag_is_newton)
-    s[1] = compoundQuadratureFormulas(math.ceil(h_opt / l), pointsPerSection, flag_is_newton)
-    s[0] = compoundQuadratureFormulas(math.ceil(h_opt / (l * l)), pointsPerSection, flag_is_newton)
-    m = processAitken(s[0], s[1], s[2], l)
-    h_opt = math.ceil(ruleRunge(m, l, s[0], s[1], h_opt / (l * l), eps))
+    s.append(compoundQuadratureFormulas(math.ceil(h_opt / (l * l)), pointsPerSection, flag_is_newton))
+    s.append(compoundQuadratureFormulas(math.ceil(h_opt / l), pointsPerSection, flag_is_newton))
+    s.append(compoundQuadratureFormulas(h_opt, pointsPerSection, flag_is_newton))
+    curr = processAitken(s[-3], s[-2], s[-1], l)
+    if not math.isnan(curr):
+        m.append(curr)
+        cm1.append((s[-2] - s[-3]) / ((1 - l ** (-m[-1])) * (math.ceil(h_form / 0.5)) ** m[-1]))
+        cm2.append((s[-1] - s[-2]) / ((1 - l ** (-m[-1])) * (math.ceil((h_form * l) / 0.5)) ** m[-1]))
+    else:
+        m.append(0)
+        cm1.append(0)
+        cm2.append(0)
+    h_opt = math.ceil(ruleRunge(m[-1], l, s[-3], s[-2], h_opt / (l * l), eps))
+    h.append(h_opt)
     print(h_opt)
-    s[2] = compoundQuadratureFormulas(h_opt, pointsPerSection, flag_is_newton)
-    return s[2]
-print(optimalIntegral(0.25, 2, 1, 1e-9) - integralCorrectMean)
+    print(h)
+    print(cm1)
+    print(cm2)
+    s.append(compoundQuadratureFormulas(math.ceil(h_opt / (l * l)), pointsPerSection, flag_is_newton))
+    s.append(compoundQuadratureFormulas(math.ceil(h_opt / l), pointsPerSection, flag_is_newton))
+    s.append(compoundQuadratureFormulas(h_opt, pointsPerSection, flag_is_newton))
+    figure, axis = plt.subplots(2, 3)
+    axis[0, 0].plot(h, m)
+    axis[0, 0].set_title('m')
+    axis[0, 1].plot(h, cm1)
+    axis[0, 1].set_title('cm h1h2')
+    axis[0, 2].plot(h, cm2)
+    axis[0, 2].set_title('cm h2h3')
+    axis[1, 0].plot(h, s[::3])
+    axis[1, 0].set_title('s h1')
+    axis[1, 1].plot(h, s[1::3])
+    axis[1, 1].set_title('s h2')
+    axis[1, 2].plot(h, s[2::3])
+    axis[1, 2].set_title('s h3')
+    plt.show()
+    return s[-1]
+print(optimalIntegral(0.25, 2, 0, 1e-9) - integralCorrectMean)
 
 #print(quadratureFormulaGauss(1.8, 2.3, 7)-integralCorrectMean)
 #printCompoundQF(100)
